7bfb5658245ef6b5ce69f889445b8f5b568353c9
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2026-01-24 15:26:10 +0700

    change then/catch to async/await

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
 LoadData();
-function LoadData() {
+async function LoadData() {
+    //async await 
     //HTTP Request GET, GET1, PUT, POST, DELETE
-    fetch('http://localhost:3000/posts').then(
-        function (data) {
-            return data.json()
+    try {
+        let res = await fetch('http://localhost:3000/posts');
+        let posts = await res.json();
+        let body = document.getElementById('post-body')
+        body.innerHTML = "";
+        for (const post of posts) {
+            body.innerHTML += convertDataToHTML(post);
         }
-    ).then(
-        function (posts) {
-            let body = document.getElementById('post-body')
-            body.innerHTML = "";
-            for (const post of posts) {
-                body.innerHTML += convertDataToHTML(post);
-            }
-        }
-    ).catch(
-        function (err) {
-            console.log(err);
-        }
-    )
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 function convertDataToHTML(post) {
     return `<tr>
@@ -27,68 +23,56 @@ function convertDataToHTML(post) {
         <td><input type='submit' value='delete' onclick='Delete(${post.id})'/><td>
     </tr>`
 }
-function saveData() {
+async function saveData() {
     let id = document.getElementById("id_txt").value;
     let title = document.getElementById("title_txt").value;
     let view = document.getElementById('views_txt').value;
-    fetch('http://localhost:3000/posts/' + id).then(
-        function (res) {
-            if (res.ok) {
-                fetch('http://localhost:3000/posts/'+id, {
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(
-                        {
-                            title: title,
-                            views: view
-                        })
-                }).then(function (res) {
-                    if (res.ok) {
-                        console.log("thanh cong");
-                    }
-                }).catch(function (err) {
-                    console.log(err);
+    let resGET = await fetch('http://localhost:3000/posts/' + id)
+    if (resGET.ok) {
+        let resPUT = await fetch('http://localhost:3000/posts/' + id, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(
+                {
+                    title: title,
+                    views: view
                 })
-                return false;
-            } else {
-                //POST
-                fetch('http://localhost:3000/posts', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(
-                        {
-                            id: id,
-                            title: title,
-                            views: view
-                        })
-                }).then(function (res) {
-                    if (res.ok) {
-                        console.log("thanh cong");
-                    }
-                }).catch(function (err) {
-                    console.log(err);
-                })
-                return false;
-            }
+        });
+        if (resPUT.ok) {
+            console.log("thanh cong");
         }
-    )
+        return false;
+    } else {
+        //POST
+        let resPOST = await fetch('http://localhost:3000/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(
+                {
+                    id: id,
+                    title: title,
+                    views: view
+                })
+        })
+        if (resPOST.ok) {
+            console.log("thanh cong");
+        }
+        return false;
+    }
 
 
 
 }
-function Delete(id) {
-    fetch('http://localhost:3000/posts/' + id, {
+async function Delete(id) {
+    let res = await fetch('http://localhost:3000/posts/' + id, {
         method: "delete"
-    })
-        .then(function (res) {
-            if (res.ok) {
-                console.log("xoa thanh cong");
-                LoadData();
-            }
-        })
-
+    });
+    if (res.ok) {
+        console.log("xoa thanh cong");
+        LoadData();
+    }
 }
