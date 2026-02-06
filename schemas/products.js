0fc2e4f2f324d574e4887c73867504043129db47
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2026-02-06 16:10:26 +0700

    CRUD with DB

---
+++ schemas/products.js
@@ -0,0 +1,38 @@
+let mongoose = require('mongoose');
+
+let productSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        slug: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        price: {
+            type: Number,
+            default: 0
+        },
+        description: {
+            type: String,
+            default: ""
+        },
+        category: {
+            type: String,
+            required: true
+        },
+        images: {
+            type: [String],
+            default: ["https://smithcodistributing.com/wp-content/themes/hello-elementor/assets/default_product.png"]
+        },
+        isDeleted:{
+            type:Boolean,
+            default:false
+        }
+    }, {
+    timestamps: true
+})
+module.exports = mongoose.model('product', productSchema)
